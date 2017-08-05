71cbbf6d0a948a1febb323df2527b69bc4ae9d57
Ajeet Kumar <ajeet.ajeetkumar@gmail.com>
2017-08-05 23:25:35 +0530

    got the algo working by reducing the throttle and playing with p,d values keeping i zero

--- src/PID.cpp
+++ src/PID.cpp
@@ -10,12 +10,26 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double pKp, double pKi, double pKd) {
+
+    Kp = pKp;
+    Ki = pKi;
+    Kd = pKd;
+
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+
+    p_error = cte;
+    i_error += cte;
+    d_error = cte - p_error;
+
 }
 
 double PID::TotalError() {
+    return -1 * (Kp * p_error + Ki * i_error + Kd * d_error);
 }
 
